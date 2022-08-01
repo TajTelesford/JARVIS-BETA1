203ea7b886d3b89b4a26bc168c9774af2e5af390
TajTe <TajTelesford@gmail.com>
2022-08-01 15:37:42 -0700

    Jarvis able to fetch stocks and able to save it to csv file, removed async properties from control center function

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-
-import speech_recognition
 import speech_recognition as sr
 import pyttsx3
-import asyncio
+from utils import *
 
+BACKGROUND_TASKS = set()
 
 engine = pyttsx3.init()
 engine.say("I AM JARVIS, YOUR HELPER")
@@ -13,21 +11,20 @@ engine.runAndWait()
 
 def wait_for_res():
     listener = sr.Recognizer()
-    while True:
-        try:
-            with sr.Microphone() as mic:
-                engine.say("Ready to listen in 3.. 2.. 1..")
-                engine.runAndWait()
-                listener.adjust_for_ambient_noise(mic, duration=0.2)
-                voice = listener.listen(mic)
-                command = listener.recognize_google(voice)
-                command = command.lower()
 
-                return command
+    try:
+        with sr.Microphone() as mic:
+            engine.say("Ready")
+            engine.runAndWait()
+            voice = listener.listen(mic)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            print(command)
 
-        except speech_recognition.UnknownValueError():
-            listener = speech_recognition()
-            continue
+            return command
+
+    except:
+        return None
 
 
 def talk(voice):
@@ -37,20 +34,32 @@ def talk(voice):
 
 def control_center(command):
     if 'jarvis' in command:
-        command = command.replace('jarvis', '')
-        print(command)
+        print("command heard")
+        if 'stocks' in command:
+            talk("Getting Stocks, give me a second to fetch data...")
+            get_stocks()
+            talk("Stocks are ready for you sir")
+
+        if 'hello' in command:
+            talk("Hello Taj")
+    elif "" in command:
+        talk("Say Something Please")
+
     else:
         engine.say("I don't know what to do bitch")
 
 
 async def main():
     control = True
-    action = ""
+    count = 0
 
     while control:
+        print(count)
         action = wait_for_res()
+        if action is None:
+            continue
         control_center(action)
+        count += 1
 
 
 asyncio.run(main())
-
--- utils.py
+++ utils.py
@@ -5,7 +5,12 @@ import re
 import time
 import asyncio
 from tqdm import tqdm, trange
+import csv
+from os.path import exists
 
+# STOCKS
+
+#############################################################################################################
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
@@ -23,7 +28,7 @@ def figure_id(div):
 
 
 # getting page source
-async def get_page_source(url):
+def get_page_source(url):
     d = webdriver.Chrome('C:/Users/TajTe/Desktop/webdriver/chromedriver.exe', options=options)
     d.get(url)
     time.sleep(1.5)
@@ -34,17 +39,18 @@ async def get_page_source(url):
 
 
 # getting the figure's stock trades/types/dates
-async def figure_stock(idlist):
+def figure_stock(idlist):
     figureUrl = "https://www.capitoltrades.com/politicians/"
     Stock_List = []
     Stock_Date = []
     Stock_Type = []
+    Stock_buy = []
+    Stock_sell = []
 
-    for i in tqdm(range(len(idlist)), desc="LOADING STOCKS>>>", maxinterval=100):
+    for i in tqdm(range(3), desc="LOADING STOCKS>>>", maxinterval=100):
         url = figureUrl + f"{idlist[i]}"
 
-        task = asyncio.create_task(get_page_source(url))
-        page = await task
+        page = get_page_source(url)
 
         doc = BeautifulSoup(page, "html.parser")
         stock = doc.find_all("h3", {"class": "q-fieldset issuer-name"})
@@ -60,22 +66,55 @@ async def figure_stock(idlist):
             Stock_Type.append(typeofstock.string)
 
         for (s, t, d) in zip(Stock_List, Stock_Type, Stock_Date):
-            print(f"{s} {t} {d}")
+            if t == "buy":
+                Stock_buy.append(f"{s} {t} {d}")
+            if t == "sell":
+                Stock_sell.append(f"{s} {t} {d}")
+
+    return Stock_buy, Stock_sell
 
 
-async def main():
+def dump_data_csv(buy, sell):
+    data = stock_buy_sell_csv()
+    data_list = []
+
+    if if_file_exists(data):
+        return
+    else:
+        for i in trange(len(buy)):
+            data_list.append(("BUY", buy[i]))
+        for i in trange(len(sell)):
+            data_list.append(("SELL", sell[i]))
+
+        with open(data, 'w') as file:
+
+            csv_write = csv.writer(file)
+            print("DUMPING DATA")
+            csv_write.writerows(data_list)
+            file.close()
+
+
+def get_stocks():
     timePeriod = 30
     url = f"https://www.capitoltrades.com/politicians?txDate={timePeriod}d"
 
     page = requests.get(url).text
     document = BeautifulSoup(page, "html.parser")
 
-    div = document.find(class_="q-cardlist__content")
-    # # ContentCard = div.find_all(name='h2')
-    # # print(ContentCard)
-    # ID_LIST = figure_id(div)
-    # figure_stock(ID_LIST, driver)
-    await figure_stock(figure_id(div))
+    ids_for_pages = document.find(class_="q-cardlist__content")
+    buy_list, sell_list = figure_stock(figure_id(ids_for_pages))
+    print(buy_list)
+    print(sell_list)
+    dump_data_csv(buy_list, sell_list)
 
 
-asyncio.run(main())
+#######################################################################################################
+
+
+def if_file_exists(path):
+    file_exists = exists(path)
+    return file_exists
+
+
+def stock_buy_sell_csv():
+    return "C:/Users/TajTe/PycharmProjects/pythonProject/StockData/stockdata.csv"
